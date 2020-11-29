61d285e4015ae07a160f75ac42c473841dc1e1fc
MNaguib2 <73434035+MNaguib2@users.noreply.github.com>
2020-11-30 01:17:14 +0200

    first Admin

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,3 @@
-
-
-
 var discription = $("p.description");
 var limit2 = 100;
 var discriptions = [];
@@ -58,3 +55,59 @@ for (var i = 0; i< discription.length; i++){
   //console.log(this.isContentToggled);
   //*/
 }
+$(document).ready(function() {
+  $(".vertical-center-4").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 4,
+    slidesToScroll: 2
+  });
+  $(".vertical-center-3").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  $(".vertical-center-2").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+    slidesToShow: 2,
+    slidesToScroll: 2
+  });
+  $(".vertical-center").slick({
+    dots: true,
+    vertical: true,
+    centerMode: true,
+  });
+  $(".vertical").slick({
+    dots: true,
+    vertical: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  $(".center").slick({
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 3
+  });
+  $(".variable").slick({
+    dots: true,
+    infinite: true,
+    variableWidth: true
+  });
+  $(".lazy").slick({
+    lazyLoad: 'ondemand', // ondemand progressive anticipated
+    infinite: true
+  });
+});
