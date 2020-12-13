fb025ae42ed9be37cbb092c48faa573e685e3e38
MNaguib2 <73434035+MNaguib2@users.noreply.github.com>
2020-12-13 14:19:35 +0200

    six commit

--- Admin/scriptadmin.js
+++ Admin/scriptadmin.js
@@ -10,11 +10,29 @@ function edite(id) {
         click = false;
         document.getElementById("Table_id").rows[rowid].style.border = "thick solid #0000FF";
     }
-    else{
+    else {
         click = true;
         $('#' + id.id).html('<i class="fa fa-edit"></i> edite');
         document.getElementById("Table_id").rows[rowid].contentEditable = "false";
-        console.log(document.getElementById("Table_id").rows[rowid])
+        console.log(document.getElementById("Table_id").rows[rowid].cells[0].innerHTML)
+        console.log(document.getElementById("Table_id").rows[rowid].cells[1].innerHTML)
+        console.log(document.getElementById("Table_id").rows[rowid].cells[2].innerHTML)
         document.getElementById("Table_id").rows[rowid].style.border = "none";
+
+        
     }
 }
+
+$("#Addcategory").click(function () {
+    var name = prompt("Please enter Name Category");
+    var Discription = prompt("Please enter Discription Category");
+    if (name.length > 1 && Discription.length > 1) {
+        location = location['C:/Users/mena_/Desktop/final%20task%20YAt/Admin/Admin%20Page.html']
+        location.reload();
+    } else {
+        alert("Your data enter error please try again")
+    }
+});
+
+
+//i will use alrt to input date from user
